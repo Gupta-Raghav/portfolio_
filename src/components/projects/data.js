7e5f6034d2aa419c8a9be1a675107dac41da9cce
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2024-02-02 12:08:11 -0500

    data update

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         title: "UI/UX Web Redesign",
         description: "Worked within a four-person team to overhaul a client’s website using Figma, enhancing user experience and achieving a 20% weekly increase in site engagement. Executed and led 16 user interviews in four distinct phases, pinpointing and addressing 5 principal challenges on the client’s website.",
-        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/manipal.webp",
+        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/MUJ.webp",
         link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
         color: "#CFF07C",
         year : "2023",
@@ -11,7 +11,7 @@ export const projects = [
     {
         title: "Poly Rythms",
         description: "I crafted a web application that explores the intricate interplay of multiple rhythms that do not conform to a single meter. This project, built entirely with vanilla JavaScript, demanded a deep dive into mathematical concepts to accurately model the simultaneous execution of diverse rhythmic patterns.",
-        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/test.png",
+        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/Poly.webp",
         link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
         color: "#CAD5D1",
         year : "2022",
@@ -21,7 +21,7 @@ export const projects = [
     {
         title: "Event management system for Manipal University Jaipur",
         description: "Built an event management system for Manipal University Jaipur incorporating React JS, Firebase, reducing permission processing time by 90% and serving 5000+ students, 10 departments, and 150 faculty.",
-        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/manipal.webp",
+        src: "https://github.com/Gupta-Raghav/portfolio_/raw/main/src/Assets/LPRC.webp",
         link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
         color: "#b5824c",
         year : "2021",
